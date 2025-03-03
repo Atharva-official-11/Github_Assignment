e60c60e8750333696ea8e96ea2ccad7bd286a578
Atharva-official-11 <levidemo1234@gmail.com>
2025-03-03 15:02:36 +0530

    commit
    assignment 6

---
+++ Day6/Task2/Program.cs
@@ -0,0 +1,38 @@
+﻿using Task2.Model;
+
+namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Events events = new Events();
+
+            events.RegisterForWDWorkshop(1684, new Student("A", "BE"));
+            events.RegisterForWDWorkshop(1685, new Student("B", "BE")); 
+            events.RegisterForWDWorkshop(1686, new Student("C", "BE"));
+            events.RegisterForWDWorkshop(1685, new Student("D", "BE"));
+            events.RegisterForWDWorkshop(1687, new Student("E", "BE"));
+
+            events.DisplayStudentListOfWDWorkshop();
+
+            events.RegisterForUIUXWorkshop(1685, new Student("F", "BE"));
+            events.RegisterForUIUXWorkshop(1684, new Student("G", "BE"));
+            events.RegisterForUIUXWorkshop(1685, new Student("H", "BE"));
+            events.RegisterForUIUXWorkshop(1686, new Student("I", "BE"));
+            events.RegisterForUIUXWorkshop(1687, new Student("J", "BE"));
+
+            events.DisplayStudentListOfUIUXWorkshop();
+
+
+            events.RegisterForRoboticsAutomationWorkshop(1687, new Student("K", "BE"));
+            events.RegisterForRoboticsAutomationWorkshop(1684, new Student("L", "BE"));
+            events.RegisterForRoboticsAutomationWorkshop(1685, new Student("M", "BE"));
+            events.RegisterForRoboticsAutomationWorkshop(1686, new Student("N", "BE"));
+            events.RegisterForRoboticsAutomationWorkshop(1685, new Student("Q", "BE"));
+
+            events.DisplayStudentListOfRoboticsAutomationWorkshop();
+
+        }
+    }
+}
