bdbfc32a45ed8332f766f5d5013e91a539717e58
Atharva-official-11 <levidemo1234@gmail.com>
2025-03-04 17:40:33 +0530

    commit
    updated

---
+++ day7/Task1/Program.cs
@@ -0,0 +1,13 @@
+﻿using Task1.Model;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee(13569,"Atharva Chaudhari",23, new DateTime(2018,01,19));
+            employee.calculateExperience();
+        }
+        }
+}
