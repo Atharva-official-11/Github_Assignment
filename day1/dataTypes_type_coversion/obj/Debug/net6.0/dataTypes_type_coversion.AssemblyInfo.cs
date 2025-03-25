6d5be319188ed2c8c5b364570cf4d7d29d815260
Atharva-official-11 <levidemo1234@gmail.com>
2025-03-26 00:16:20 +0530

    commit

--- day1/dataTypes_type_coversion/obj/Debug/net6.0/dataTypes_type_coversion.AssemblyInfo.cs
+++ day1/dataTypes_type_coversion/obj/Debug/net6.0/dataTypes_type_coversion.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dataTypes_type_coversion")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5685fe19a7670fdb0bd66fb7ba6e09bf70e9e9d")]
 [assembly: System.Reflection.AssemblyProductAttribute("dataTypes_type_coversion")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dataTypes_type_coversion")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
